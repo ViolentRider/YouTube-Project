c51520dfc17ccfd2de91064b68d9a1e04952e314
Bartosz <104717320+bartosz-bednarski@users.noreply.github.com>
2022-11-14 14:50:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const USER_ID =
   "286529760252-s3j7glfmn64jrp2vbkurv790g013qc0q.apps.googleusercontent.com";
 const DISCOVERY_DOCS =
   "https:www.googleapis.com/discovery/v1/apis/youtube/v3/res";
-const SCOPES = "https://www.googleapis.com/auth/youtube.readonly";
 
 const client = google.accounts.oauth2.initTokenClient({
   client_id:
@@ -13,6 +12,7 @@ const client = google.accounts.oauth2.initTokenClient({
   callback: (tokenResponse) => {
     client.requestAccessToken();
     client.ACCESS_TOKEN = `${tokenResponse.access_token}`;
+    createChannelListIcons();
   },
 });
 
@@ -27,8 +27,6 @@ async function startAxios() {
   };
   const channelRes = await axios(target);
   let resChannellToArray = [channelRes];
-  console.log(client.ACCESS_TOKEN);
-  console.log(channelRes);
 
   return resChannellToArray;
 }
@@ -38,9 +36,7 @@ function main() {
 
 async function getData() {
   const resChannellToArray = await startAxios();
-  console.log(resChannellToArray);
   let CHANNEL_ID = await resChannellToArray[0].data.items[0].id;
-  console.log(CHANNEL_ID);
   const getYTChannelData = {
     method: "get",
     url: `https://youtube.googleapis.com/youtube/v3/subscriptions?part=snippet%2CcontentDetails&channelId=${CHANNEL_ID}&maxResults=100&key=${API_KEY}`,
@@ -52,30 +48,19 @@ async function getData() {
 
   let res = await axios(getYTChannelData);
   let resArray = [...res.data.items];
-  console.log(resArray);
   const resToArray = [];
-  ////Tu ma być długość obiektu
   for (i = 0; i < resArray.length; i++) {
     resToArray.push(res.data.items[i].snippet);
   }
   return resToArray;
 }
-// async function createChannelDataArray() {
-//   let res = await getData();
-//   const resToArray = [];
-//   for (i = 0; i <= 49; i++) {
-//     resToArray.push(res.data.items[i].snippet);
-//   }
-//   return resToArray;
-// }
 
 async function createChannelListIcons() {
   const resToArray = await getData();
-  console.log(resToArray);
   const topSideChannelListBox = document.querySelector(
     ".top-side-channel-list"
   );
-
+  topSideChannelListBox.innerHTML = "";
   for (i = 0; i < resToArray.length; i++) {
     const channelIcon = document.createElement("img");
     topSideChannelListBox.append(channelIcon);
@@ -102,117 +87,117 @@ function checkTopside(e) {
 }
 topSideContainer.addEventListener("click", checkTopside);
 
-async function getChannelsVideos() {
-  const topSideChannelListBox = document.querySelector(
-    ".top-side-channel-list"
-  );
+async function findChannelVideos(e) {
+  const URL =
+    "https://www.googleapis.com/youtube/v3/search?key=" +
+    API_KEY +
+    "&channelId=" +
+    e.target.getAttribute("id") +
+    "&part=snippet,id&order=date&maxResults=20";
+  const res = await axios.get(URL);
+  const videos = [res.data.items];
 
-  const channelsIdName = topSideChannelListBox.querySelectorAll("img");
-  const channelsArray = [];
-  channelsIdName.forEach((item) => {
-    let item1 = item.getAttribute("class");
-    let item2 = item.getAttribute("id");
-
-    let channelName = {
-      channelName: `${item1}`,
-      channelId: `${item2}`,
-    };
-    channelsArray.push(channelName);
-  });
-  const channelsVideos = [];
-  for (i = 0; i < 2; i++) {
-    let URL =
-      "https://www.googleapis.com/youtube/v3/search?key=" +
-      API_KEY +
-      "&channelId=" +
-      channelsArray[i].channelId +
-      "&part=snippet,id&order=date&maxResults=10";
-    let axiosRes = await axios.get(URL);
-    console.log(axiosRes);
-    channelsVideos.push(
-      [axiosRes.data.items[0].snippet.channelTitle][
-        axiosRes.data.items[0].snippet
-      ]
-    );
-  }
-
-  console.log(channelsArray);
+  console.log(videos);
+  return videos;
 }
-getChannelsVideos();
-// async function findChannelVideos(e) {
 
-//   const URL =
-//     "https://www.googleapis.com/youtube/v3/search?key=" +
-//     API_KEY +
-//     "&channelId=" +
-//     e.target.getAttribute("id") +
-//     "&part=snippet,id&order=date&maxResults=20";
-//   const res = await axios.get(URL);
-//   const videos = [res.data.items];
-//   console.log(videos);
-//   return videos;
-// }
+async function youtubeIframeAPI() {
+  // 2. This code loads the IFrame Player API code asynchronously.
+  var tag = document.createElement("script");
 
-// async function youtubeIframeAPI() {
-//   // 2. This code loads the IFrame Player API code asynchronously.
-//   var tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
 
-//   tag.src = "https://www.youtube.com/iframe_api";
-//   var firstScriptTag = document.getElementsByTagName("script")[0];
-//   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-// }
+let todayDate = new Date();
+let todayDayToString = todayDate.toDateString();
+let todayDay = todayDayToString.slice(8, 10);
+let todayMonth = todayDate.getMonth();
+let todayYear = todayDate.getFullYear();
 
-// // 3. This function creates an <iframe> (and YouTube player)
-// //    after the API code downloads.
+async function createVideoBox(e) {
+  const videos = await findChannelVideos(e);
+  const lastWeekVideosContainer = document.querySelector(
+    ".last-week-videos-container"
+  );
+  for (i = 0; i <= 15; i++) {
+    let videoDate = new Date(`${videos[0][i].snippet.publishTime}`);
+    let videoDay = videos[0][i].snippet.publishTime.slice(8, 10);
+    let videoMonth = videoDate.getMonth();
+    let videoYear = videoDate.getFullYear();
 
-// async function createVideoBox(e) {
-//   const videos = await findChannelVideos(e);
-//   console.log(videos);
-//   for (i = 0; i <= 1; i++) {
-//     const videosContainer = document.querySelector(".videos-container");
-//     const videoBox = document.createElement("div");
-//     videoBox.classList.add("video-box");
-//     videosContainer.append(videoBox);
+    if (
+      (todayDay >= videoDay) &
+      (videoDay >= videoDay - 7) &
+      (videoMonth == todayMonth) &
+      (videoYear == todayYear)
+    ) {
+      const videoBox = document.createElement("div");
+      videoBox.classList.add("video-box");
+      lastWeekVideosContainer.append(videoBox);
 
-//     const playerDiv = document.createElement("div");
-//     playerDiv.classList.add("video-img");
-//     playerDiv.style.backgroundImage = `url(${videos[0][i].snippet.thumbnails.high.url})`;
-//     playerDiv.setAttribute("id", videos[0][i].id.videoId);
-//     videoBox.append(playerDiv);
+      const playerDiv = document.createElement("div");
+      playerDiv.classList.add("video-img");
+      playerDiv.style.backgroundImage = `url(${videos[0][i].snippet.thumbnails.high.url})`;
+      playerDiv.setAttribute("id", videos[0][i].id.videoId);
+      videoBox.append(playerDiv);
 
-//     const videoInfo = document.createElement("div");
-//     videoInfo.classList.add("video-info");
-//     videoBox.append(videoInfo);
+      const videoInfo = document.createElement("div");
+      videoInfo.classList.add("video-info");
+      videoBox.append(videoInfo);
 
-//     const videoTitle = document.createElement("h3");
-//     let item = document.querySelector(
-//       `.${videos[0][0].snippet.channelTitle
-//         .replace(/\./g, "")
-//         .replace(/\s+/g, "")}`
-//     );
-//     // console.log(item.getAttribute("src"));
-//     videoTitle.textContent = videos[0][i].snippet.title;
-//     videoInfo.append(videoTitle);
+      const videoTitle = document.createElement("h3");
+      let item = document.querySelector(
+        `.${videos[0][0].snippet.channelTitle
+          .replace(/\./g, "")
+          .replace(/\s+/g, "")}`
+      );
+      videoTitle.textContent = videos[0][i].snippet.title;
+      videoInfo.append(videoTitle);
 
-//     const author = document.createElement("p");
-//     author.innerHTML = `<img src = '${item.getAttribute("src")}'> ${
-//       videos[0][i].snippet.channelTitle
-//     }`;
-//     author.style;
-//     videoInfo.append(author);
+      const author = document.createElement("p");
+      author.innerHTML = `<img src = '${item.getAttribute("src")}'> ${
+        videos[0][i].snippet.channelTitle
+      }`;
+      author.style;
+      videoInfo.append(author);
+    } else {
+      const olderVideosContainer = document.querySelector(
+        ".older-videos-container"
+      );
+      const videoBox = document.createElement("div");
+      videoBox.classList.add("video-box");
+      olderVideosContainer.append(videoBox);
 
-//     const publishingdate = document.createElement("p");
-//     publishingdate.textContent = videos[0][i].snippet.publishTime.slice(0, 10);
-//     videoInfo.append(publishingdate);
+      const playerDiv = document.createElement("div");
+      playerDiv.classList.add("video-img");
+      playerDiv.style.backgroundImage = `url(${videos[0][i].snippet.thumbnails.high.url})`;
+      playerDiv.setAttribute("id", videos[0][i].id.videoId);
+      videoBox.append(playerDiv);
 
-//     // videos[0][i].id.videoId = new YT.Player(`${videos[0][i].id.videoId}`, {
-//     //   videoId: videos[0][i].id.videoId,
-//     // });
-//   }
-// }
+      const videoInfo = document.createElement("div");
+      videoInfo.classList.add("video-info");
+      videoBox.append(videoInfo);
 
-const inputIdBtn = document.querySelector(".yt-id-input-btn");
-inputIdBtn.addEventListener("click", createChannelListIcons);
+      const videoTitle = document.createElement("h3");
+      let item = document.querySelector(
+        `.${videos[0][0].snippet.channelTitle
+          .replace(/\./g, "")
+          .replace(/\s+/g, "")}`
+      );
+      videoTitle.textContent = videos[0][i].snippet.title;
+      videoInfo.append(videoTitle);
+
+      const author = document.createElement("p");
+      author.innerHTML = `<img src = '${item.getAttribute("src")}'> ${
+        videos[0][i].snippet.channelTitle
+      }`;
+      author.style;
+      videoInfo.append(author);
+    }
+  }
+}
 
 function displayVideo(e) {
   if (e.target.matches(".video-img")) {
@@ -260,8 +245,9 @@ function scrollMouse() {
     scrollContainer.scrollLeft += evt.deltaY;
   });
 }
+const loginBtn = document.querySelector(".yt-id-input-btn");
+loginBtn.addEventListener("click", () => {
+  client.requestAccessToken();
+});
 document.addEventListener("click", displayVideo);
 document.addEventListener("DOMContentLoaded", main);
-
-//pobiera id z inputu ; przekierowac response do funkcji pozostalych !!!!!!!!!!!!!!!!!!!
-//UCaAqWToMsQFViH2dufxwN7Q
